c37057b0315b82de988787fa7b638e789a94825b
Edder Ramírez <edderleonardo@gmail.com>
2024-05-19 00:45:09 -0600

    add create Task

--- yuhutodo/apps/todo/views.py
+++ yuhutodo/apps/todo/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class TodoView(TemplateView):
+class TodoView(LoginRequiredMixin, TemplateView):
     template_name = "pages/todo.html"
